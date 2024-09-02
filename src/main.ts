af3124aba1a4c2f857b7166385b4d9cfc6fedb9d
Tran Thinh <thinh221201@gmail.com>
2024-09-02 16:40:39 +0700

    feat: :rocket: swagger api document

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Create Swagger API documentation
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   app.setGlobalPrefix('api');
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(8000);
 }
 bootstrap();
