64b891d8596a5fbcf7aa553ae52bd6da5db5a1dc
Suzume <suzume.logs@gmail.com>
2024-09-14 17:17:42 +0700

    Update

--- src/_dtos/update_book.dto.ts
+++ src/_dtos/update_book.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookDto } from './create_book.dto';
-import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {
-  @IsOptional()
-  @IsBoolean()
-  isPremium?: boolean;
-}
+export class UpdateBookDto extends PartialType(CreateBookDto) {}
