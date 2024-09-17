112877967518f884df4619a25b9f7b4ecdf1767e
CuongThieu <cuongtt@solashi.com>
2024-09-17 11:28:02 +0700

    Update

--- src/_dtos/update_chapter.dto.ts
+++ src/_dtos/update_chapter.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateChapterDto } from './create_chapter.dto';
-import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateChapterDto extends PartialType(CreateChapterDto) {
-  @IsOptional()
-  @IsBoolean()
-  isPremium?: boolean;
-}
+export class UpdateChapterDto extends PartialType(CreateChapterDto) {}
