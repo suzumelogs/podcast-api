ff10fa838e0332944540444af2bb28d35f893ba4
Suzume <suzume.logs@gmail.com>
2024-10-21 20:30:22 +0700

    Update

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -3,49 +3,49 @@ import {
   Post,
   UseInterceptors,
   UploadedFile,
+  BadRequestException,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { ImageTypeValidationPipe } from 'src/pipes/image-type-validation.pipe';
 import { AudioTypeValidationPipe } from 'src/pipes/audio-type-validation.pipe';
+import { extname } from 'path';
+
+const storageConfig = (destinationPath: string) => ({
+  storage: diskStorage({
+    destination: destinationPath,
+    filename: (req, file, callback) => {
+      const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+      const extension = extname(file.originalname);
+      const filename = `${uniqueSuffix}${extension}`;
+      callback(null, filename);
+    },
+  }),
+});
 
 @Controller('uploads')
 export class UploadsController {
   @Post('image')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './storage/images/',
-        filename: (req, file, callback) => {
-          const filename = Date.now() + '-' + file.originalname;
-          callback(null, filename);
-        },
-      }),
-    }),
-  )
+  @UseInterceptors(FileInterceptor('file', storageConfig('./storage/images/')))
   uploadImage(
     @UploadedFile(new ImageTypeValidationPipe())
     file: Express.Multer.File,
   ) {
-    return file.path?.replace('\\', '/');
+    if (!file) {
+      throw new BadRequestException('Image upload failed');
+    }
+    return { path: file.path?.replace(/\\/g, '/') };
   }
 
   @Post('audio')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './storage/audios',
-        filename: (req, file, callback) => {
-          const filename = Date.now() + '-' + file.originalname;
-          callback(null, filename);
-        },
-      }),
-    }),
-  )
+  @UseInterceptors(FileInterceptor('file', storageConfig('./storage/audios/')))
   uploadAudio(
     @UploadedFile(new AudioTypeValidationPipe())
     file: Express.Multer.File,
   ) {
-    return file.path?.replace('\\', '/');
+    if (!file) {
+      throw new BadRequestException('Audio upload failed');
+    }
+    return { path: file.path?.replace(/\\/g, '/') };
   }
 }
