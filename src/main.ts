6232c2074bce68484c4c7ac4f935fbbf36acc6fe
dinhphamcanh <dinhpc@solashi.com>
2024-12-19 17:36:12 +0700

    feat: update api

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
   app.setGlobalPrefix('api');
   app.enableCors({
